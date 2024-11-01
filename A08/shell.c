9c44e1769d936dd2231360452821939d50cceaec
Kabir Alvaro Hinduja-Obregon <khindujaob@goldengate.cs.brynmawr.edu>
2024-11-01 17:17:39 -0400

    assignment complete!

--- A08/shell.c
+++ A08/shell.c
@@ -1,5 +1,73 @@
+/*----------------------------------------------
+ * Author: Kabir Alvaro Hinduja-Obregon 
+ * Date: 1/11/24
+ * Description: My own personal and customized
+ * very simple shell made from scratch! 
+ ---------------------------------------------*/
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pwd.h>
+#include <sys/wait.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+void print_intro() {
+	char cwd[64];
+	struct passwd *pw = getpwuid(geteuid());
+	if (getcwd(cwd, sizeof(cwd)) != NULL){
+		printf(ANSI_COLOR_MAGENTA "KabirSuperPinkShell %s %s " ANSI_COLOR_RESET, cwd, pw->pw_name);
+	}
+	else {
+		perror("cwd or pwuid error");
+		exit(EXIT_FAILURE);
+	}
+}
+
+void command_execution(char *command){
+	char *args[64];
+	char *token = strtok(command, " ");
+	int i = 0;
+	while (token != NULL){
+		args[i++] = token;
+		token = strtok(NULL, " ");
+	}
+	args[i] = NULL;
+	pid_t pid = fork();
+	if (pid == 0) {
+		if (execvp(args[0], args) == -1){
+			perror("Failed to execute the command");
+			exit(EXIT_FAILURE);
+		}
+	} else {
+		int status;
+		wait(&status);
+		}
+	} 
+
 
 int main() {
+	char *input;
+	while(1) {
+		print_intro();
+		input = readline(NULL);
+		if (input == NULL){
+			printf("\n");
+			continue;
+		}
+		if (strcmp(input, "exit") == 0){
+			free(input);
+			break;
+		}
+		add_history(input);
+		command_execution(input);
+		free(input);
+
+	}
   return 0;
 }
--- A08/warmup.c
+++ A08/warmup.c
@@ -1,5 +1,49 @@
+/*----------------------------------------------
+ * Author: Kabir Alvaro Hinduja-Obregon 
+ * Date: 1/11/24
+ * Description: A warmup to create children with 
+ * fork that print pid along with different 
+ * alphabet and numerical combinations . 
+ ---------------------------------------------*/
+
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 int main() {
+	pid_t pid;
+	int status;
+	printf("%d", getpid());
+	printf("]  A0\n");
+	pid = fork();
+	if (pid != 0){
+		 printf("%d", getpid());
+        	 printf("]  B0\n");
+		 }
+	else {
+		printf("%d", getpid());
+		printf("]  B1\n");
+		printf("%d", getpid());
+                printf("]  Bye\n");
+	}
+	if (pid != 0){
+		pid = wait(&status);
+		pid = fork();
+		if (pid != 0){
+
+			printf("%d", getpid());
+                	printf("]  C0\n");
+                	printf("%d", getpid());
+                	printf("]  Bye\n");
+		}
+		if (pid == 0){
+			printf("%d", getpid());
+                	printf("]  C1\n");
+                	printf("%d", getpid());
+                	printf("]  Bye\n");
+		}
+	}
+	
+
   return 0;
 }
